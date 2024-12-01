7bb160722733057ed39121cf668c82e207ff6e4b
BennetLe <bennetleins@gmx.de>
2024-12-01 21:34:31 +0100

    added color pallet to kitty

--- home/bennet.nix
+++ home/bennet.nix
@@ -56,11 +56,35 @@
 
   programs.kitty = {
     enable = true;
+    shellIntegration.mode = "zsh";
     settings = {
         confirm_os_window_close = 0;
         enable_audio_bell = false;
         dynamic_background_opacity = true;
         background_opacity = "0.75";
+        
+        # custom gruvbox theme with no bg and fg
+        selection_foreground = "#ebdbb2";
+        selection_background = "#d65d0e";
+        color0 = "#3c3836";
+        color1 = "#cc241d";
+        color2 = "#98971a";
+        color3 = "#d79921";
+        color4 = "#458588";
+        color5 = "#b16286";
+        color6 = "#689d6a";
+        color7 = "#a89984";
+        color8 = "#928374";
+        color9 = "#fb4934";
+        color10 = "#b8bb26";
+        color11 = "#fabd2f";
+        color12 = "#83a598";
+        color13 = "#d3869b";
+        color14 = "#8ec07c";
+        color15 = "#fbf1c7";
+        cursor = "#bdae93";
+        cursor_text_color = "#665c54";
+        url_color = "#458588";
       };
   };
 
