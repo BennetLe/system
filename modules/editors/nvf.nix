d30141038a161a261589e2b2ba9efd9699c745c5
BennetLe <bennetleins@gmx.de>
2025-02-26 23:17:34 +0100

    basic nvf setup

--- modules/editors/nvf.nix
+++ modules/editors/nvf.nix
@@ -1,14 +1,11 @@
 {
-  config,
-  lib,
   pkgs,
-  nvf,
   ...
 }:
 
 {
   programs.nvf = {
-    enable = true;
+     enable = true;
 
     settings = {
       vim = {
@@ -20,9 +17,118 @@
           name = "gruvbox";
           style = "dark";
         };
+
+        visuals = {
+          nvim-web-devicons.enable = true;
+        };
+
+        ui = {
+          borders.plugins.lsp-signature.enable = true;
+        };
+
+        tabline.nvimBufferline.setupOpts.options.diagnostics_update_in_insert = true;
+
+        lazy = {
+          plugins = {
+            "guess-indent.nvim" = {
+              package = pkgs.vimPlugins.guess-indent-nvim;
+              setupModule = "guess-indent";
+            };
+            
+            "oil.nvim" = {
+              package = pkgs.vimPlugins.oil-nvim;
+              setupModule = "oil";
+            };
+
+            
+            "undotree" = {
+              package = pkgs.vimPlugins.undotree;
+
+              # load on keymap
+              keys = [
+                {
+                  key = "<leader>u";
+                  action = ":UndotreeToggle<CR>";
+                  mode = "n";
+                }
+              ];
+            };   
+          };
+        };
+
+        mini = {
+          animate.enable = true;
+          basics.enable = true;
+          comment = {
+            enable = true;
+            setupOpts = {
+              mappings = {
+                comment = "<leader>/";
+                comment_line = "<leader>/";
+                comment_visual = "<leader>/";
+                textobject = "<leader>/";
+              };
+            };
+          };
+          git.enable = true; 
+          hipatterns.enable = true;
+          icons.enable = true;
+          indentscope.enable = true;
+          move.enable = true;
+          pairs.enable = true;
+        };
+
+        snippets = {
+          luasnip.enable = true;
+        };
+
+        statusline = {
+          lualine.enable = true;
+        };
+        telescope = {
+          enable = true;
+          mappings = {
+            findFiles = "<leader>fd";
+            liveGrep = "<leader>ff";
+            helpTags = "<leader>fh";
+          };
+        };
+
+        autocomplete = {
+          blink-cmp = {
+            enable = true;
+            mappings = {
+              close = "<C-e>";
+              complete = "<C-Space>";
+              confirm = "<C-y>";
+              next = "<C-n>";
+              previous = "<C-p>";
+            };
+          };
+        };
         
         languages = {
-          nix.enable = true;
+          enableLSP = true;
+          enableTreesitter = true;
+          nix = {
+            enable = true;
+            extraDiagnostics.enable = true;
+            format.enable = true;
+          };
+          ts = {
+            enable = true;
+            format.enable = true;
+          };
+          rust = {
+            enable = true;
+            crates.enable = true;
+            dap.enable = true;
+            format.enable = true;
+          };
+          zig = {
+            enable = true;
+            dap.enable = true;
+          };
         };
       };
     };
