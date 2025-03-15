f679c093018b3f5b44c42716a30f64541221b754
BennetLe <bennetleins@gmx.de>
2025-03-15 14:27:23 +0100

    added hyprlock to home-manager

--- home/bennet.nix
+++ home/bennet.nix
@@ -336,6 +336,7 @@
         "$mainMod, 7, workspace, 7"
         "$mainMod, 8, workspace, 8"
         "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
 
         "$mainMod SHIFT, 1, movetoworkspace, 1"
         "$mainMod SHIFT, 2, movetoworkspace, 2"
@@ -346,6 +347,7 @@
         "$mainMod SHIFT, 7, movetoworkspace, 7"
         "$mainMod SHIFT, 8, movetoworkspace, 8"
         "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
       ];
 
       bindm = [
@@ -388,8 +390,151 @@
         "noblur, class:^(xwaylandvideobridge)$"
         "nofocus, class:^(xwaylandvideobridge)$"
       ];
+
+      workspace = [
+        "1,monitor:DP-1"
+        "2,monitor:DP-1"
+        "3,monitor:DP-1"
+        "4,monitor:DP-1"
+        "5,monitor:DP-2"
+        "6,monitor:DP-2"
+        "7,monitor:DP-2"
+        "8,monitor:DP-2"
+        "9,monitor:DP-2"
+        "10,monitor:DP-2"
+
+      ];
     };
   };
+
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        grace = 1;
+      };
+
+      background = {
+        path = "~/Wallpapers/Gruvbox/gruvy-night.png";
+        blur_size = 5;
+        blur_passes = 1;
+        noise = 0.0117;
+        contrast = 1.3;
+        brightness = 0.5;
+        vibrancy = 0.21;
+        vibrancy_darkness = 0.5;
+      };
+
+      input-field = {
+        size = "250, 50";
+        outline_thickness = 0;
+        dots_size = 0.2;
+        dots_spacing = 0.15;
+        dots_center = true;
+        outer_color = "rgb(126, 247, 138)";
+        inner_color = "rgb(0, 0, 0)";
+        font_color = "rgb(126, 247, 138)";
+        fade_on_empty = true;
+        placeholder_text = "<i>Password...</i>";
+        hide_input = false;
+        position = "0, 100";
+        halign = "center";
+        valign = "bottom";
+      };
+
+      label = [
+        # Date
+        {
+          monitor = "";
+          text = ''cmd[update:18000000] echo "<b> "$(date +'%A, %-d %B %Y')" </b>"'';
+          color = "rgba(184, 212, 224, 0.6)";
+          font_size = 28;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, -420";
+          halign = "center";
+          valign = "top";
+        }
+        # Time
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo -e "$(date +"%H":"%M")"'';
+          color = "rgba(184, 212, 224, 0.6)";
+          font_size = 130;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, -220";
+          halign = "center";
+          valign = "top";
+        }
+        # User
+        {
+          monitor = "";
+          text = ''$USER'';
+          color = "rgb(126, 247, 138)";
+          font_size = 16;
+          font_family = "Inter Display Medium";
+          position = "0, 70";
+          halign = "center";
+          valign = "bottom";
+        }
+        # Uptime
+        {
+          monitor = "";
+          text = ''cmd[update:60000] echo "<b> "$(uptime -p || $Scripts/UptimeNixOS.sh)" </b>"'';
+          color = "rgba(184, 212, 224, 0.4)";
+          font_size = 12;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "5, 5";
+          halign = "right";
+          valign = "bottom";
+        }
+        # # Music Player Info # #
+        # PLAYER TITLE
+        {
+          monitor = "DP-1";
+          text = ''cmd[update:1000] echo "$(playerctl metadata --format "{{ xesam:title }}" 2>/dev/null | cut -c1-50'';
+          color = "rgba(184, 212, 224, 0.8)";
+          font_size = 14;
+          font_family = "Inter Display Medium";
+          position = "180, 120";
+          halign = "left";
+          valign = "bottom";
+        }
+        # PLAYER ARTIST
+        {
+          monitor = "DP-1";
+          text = ''cmd[update:1000] echo "$(playerctl metadata --format "{{ xesam:artist }}" 2>/dev/null | cut -c1-50)"'';
+          color = "rgba(184, 212, 224, 0.8)";
+          font_size = 14;
+          font_family = "Inter Display Medium";
+          position = "180, 80";
+          halign = "left";
+          valign = "bottom";
+        }
+        # PLAYER ALBUM
+        {
+          monitor = "DP-1";
+          text = ''cmd[update:1000] echo "$(playerctl metadata --format "{{ xesam:album }}" 2>/dev/null | cut -c1-50)"'';
+          color = "rgba(184, 212, 224, 0.8)";
+          font_size = 14;
+          font_family = "Inter Display Medium";
+          position = "180, 40";
+          halign = "left";
+          valign = "bottom";
+        }
+      ];
+    };
+  };
+
+  # programs.tmux = {
+  #   enable = true;
+  #   prefix = "C-Space";
+  #
+  #   plugins = with pkgs; [
+  #     {
+  #
+  #     }
+  #   ];
+  # };
 
   programs.home-manager.enable = true;
 
