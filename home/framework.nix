214699f9431268b3a67bca850bb05991d523b3d8
BennetLe <BennetLe@proton.me>
2025-05-07 12:28:19 +0200

    added diagnostic config

--- home/framework.nix
+++ home/framework.nix
@@ -54,6 +54,8 @@
         # Set the directory we want to store zinit and plugins
         # ZINIT_HOME="''${XDG_DATA_HOME:-''${HOME}/.local/share}/zinit/zinit.git"
 
+        export PATH="''${HOME}/go/bin:$PATH"
+
         ZIM_HOME=~/.zim
 
         # Download Zinit, if it's not there yet
