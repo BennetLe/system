750e044cafb9277df5b305dd90348214bcc3e3e5
BennetLe <BennetLe@proton.me>
2025-05-23 09:49:25 +0200

    added nushell to framework

--- home/framework.nix
+++ home/framework.nix
@@ -34,6 +34,74 @@
   imports = [inputs.hyprpanel.homeManagerModules.hyprpanel];
 
   programs = {
+    zoxide = {
+      enable = true;
+      enableNushellIntegration = true;
+      enableZshIntegration = true;
+    };
+    nushell = {
+      enable = true;
+      # configFile.source = ./config.nu;
+      extraConfig = ''
+         let carapace_completer = {|spans|
+            carapace $spans.0 nushell ...$spans | from json
+          }
+          $env.config = {
+            show_banner: false,
+            completions: {
+            case_sensitive: false # case-sensitive completions
+            quick: true    # set to false to prevent auto-selecting completions
+            partial: true    # set to false to prevent partial filling of the prompt
+            algorithm: "fuzzy"    # prefix or fuzzy
+            external: {
+              # set to false to prevent nushell looking into $env.PATH to find more suggestions
+              enable: true
+              # set to lower can improve completion performance at the cost of omitting some options
+              max_results: 100
+              completer: $carapace_completer # check 'carapace_completer'
+            }
+          }
+        }
+        $env.PATH = ($env.PATH |
+        split row (char esep) |
+        prepend /home/myuser/.apps |
+        append /usr/bin/env
+        )
+      '';
+      envFile = {
+        text = ''
+          $env.TRANSIENT_PROMPT_COMMAND = ^starship module character
+        '';
+      };
+      shellAliases = {
+        # ls = "eza";
+        ll = "ls -l";
+        cls = "clear";
+        cat = "bat";
+        s = "kitten ssh";
+
+        update = "nixos-rebuild switch --use-remote-sudo --flake /home/bennet/system#bennet";
+        config = "nvim /home/bennet/system/flake.nix";
+        changewp = "swww img";
+
+        brave = "brave --password-store=gnome";
+      };
+    };
+    carapace.enable = true;
+    carapace.enableNushellIntegration = true;
+
+    starship = {
+      enable = true;
+      enableTransience = true;
+      settings = {
+        add_newline = true;
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+      };
+    };
+
     hyprpanel = {
       enable = true;
       hyprland.enable = true;
