dcb389dde6a619bc461a26158d46d5da5ea14dce
BennetLe <BennetLe@proton.me>
2025-06-06 10:06:07 +0200

    added framework key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,4 +1,5 @@
 let
   bennet = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDlQkqu/cPdgeUG3BUPqFh7yOw+tRhcCQaPwYLGXtaQy root@nixos";
+  framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGhKJD5LXtdC2C/jkjNrs749Ko/hr0tJA4sZRrfX268P bennet@nixos";
 in {
 }
