2489520d68a9dd568fce12f31ab1d64ea97366c6
BennetLe <BennetLe@proton.me>
2025-06-16 15:31:07 +0200

    added yazi shell wrapper

--- home/framework.nix
+++ home/framework.nix
@@ -67,6 +67,16 @@
         prepend /home/myuser/.apps |
         append /usr/bin/env
         )
+
+        def --env y [...args] {
+          let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+          yazi ...$args --cwd-file $tmp
+          let cwd = (open $tmp)
+          if $cwd != "" and $cwd != $env.PWD {
+            cd $cwd
+          }
+          rm -fp $tmp
+        }
       '';
       envFile = {
         text = ''
