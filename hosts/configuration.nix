8ea8ce84ece44981084148a13c7329c0751b6c1c
BennetLe <BennetLe@proton.me>
2025-08-24 10:17:21 +0200

    added exploitdb

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -111,6 +111,7 @@ in {
       nuclei
       bettercap
       socat
+      exploitdb
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
