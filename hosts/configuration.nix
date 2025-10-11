c6684ea1589b6cd4f3442b752bf5e201c8f178c5
BennetLe <BennetLe@proton.me>
2025-10-11 20:17:23 +0200

    pkgs: added mitmproxy and whatweb

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -111,12 +111,13 @@ in {
       wordlists
       seclists
       gobuster
-      # mitmproxy
+      mitmproxy
       metasploit
       nuclei
       bettercap
       socat
       exploitdb
+      whatweb
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
