ed9a8c4f67675ffffef9f5abc4e481dd835370b2
BennetLe <BennetLe@proton.me>
2025-10-11 20:35:11 +0200

    pkgs: added cewl

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -118,6 +118,7 @@ in {
       socat
       exploitdb
       whatweb
+      cewl
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
