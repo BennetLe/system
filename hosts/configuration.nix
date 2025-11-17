935accc9e475ececf80a9e0ea79a0f8067484c65
BennetLe <git@bennetle.de>
2025-11-17 16:29:48 +0100

    pkgs: added nuclei

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -123,6 +123,7 @@ in {
       aircrack-ng
       airgorah
       hashcat
+      nuclei
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
