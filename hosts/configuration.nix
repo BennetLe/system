325e995bb94c93cf3e7a49bbcb0d71f5f019e808
BennetLe <git@bennetle.de>
2026-02-04 14:33:09 +0100

    pkgs: added masscan

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -124,6 +124,7 @@ in {
       airgorah
       hashcat
       nuclei
+      masscan
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
