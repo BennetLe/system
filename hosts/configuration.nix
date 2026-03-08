b9144d1bff250bc697e648c228e952ea5aa988db
BennetLe <git@bennetle.de>
2026-03-08 15:38:26 +0100

    pkgs: added pentesting tools

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -126,6 +126,16 @@ in {
       hashcat
       nuclei
       masscan
+      routersploit
+      social-engineer-toolkit
+      sqlmap
+      thc-hydra
+      thc-ipv6
+      # yersinia
+      arp-scan
+      cherrytree
+      bettercap
+      responder
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
