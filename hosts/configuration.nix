f2ab9402158ececa3dbc5ecf1a4d7bb390ccff04
BennetLe <git@bennetle.de>
2026-06-08 17:06:13 +0200

    add bloodhound

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -179,6 +179,7 @@ in {
       sherlock
       wpscan
       netexec
+      bloodhound
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
