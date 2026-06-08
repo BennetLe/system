c3b4e3ff3af0299aefe01a45ac09e862392a0fc6
BennetLe <git@bennetle.de>
2026-06-08 16:30:13 +0200

    add netexec

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -178,6 +178,7 @@ in {
       sshuttle
       sherlock
       wpscan
+      netexec
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
