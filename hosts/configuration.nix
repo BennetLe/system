5ad5da17cc3fe8266e3dc4f6bbe0cc36c148ca3e
BennetLe <git@bennetle.de>
2026-06-09 12:04:20 +0200

    add pwncat

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -180,6 +180,7 @@ in {
       wpscan
       netexec
       bloodhound
+      pwncat
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
