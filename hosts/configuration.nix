a20941bf70697215443272e21180844974640e10
BennetLe <git@bennetle.de>
2026-06-10 09:53:38 +0200

    add zap

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -181,6 +181,7 @@ in {
       netexec
       bloodhound
       villain
+      zap
 
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
